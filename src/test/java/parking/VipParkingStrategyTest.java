5a4324ae07b719c5a7def433d584cb1f38e3f1d1
MandyChen <497133212@qq.com>
2020-08-26 17:40:11 +0800

    [mock][mandy] test : testIsAllowOverPark_givenCarNameContainsCharacterAAndIsVipCar_thenReturnTrue"

--- src/test/java/parking/VipParkingStrategyTest.java
+++ src/test/java/parking/VipParkingStrategyTest.java
@@ -1,12 +1,22 @@
 package parking;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Arrays;
 
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class VipParkingStrategyTest {
+    @Mock
+    CarDao carDao;
+    @InjectMocks
+    VipParkingStrategy vipParkingStrategy;
 
     @Test
     public void testPark_givenAVipCarAndAFullParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName() {
@@ -33,31 +43,34 @@ public class VipParkingStrategyTest {
     public void testPark_givenCarIsNotVipAndAFullParkingLog_thenGiveNoSpaceReceipt() {
 
         //given
-        VipParkingStrategy vipParkingStrategy=spy(new VipParkingStrategy());
+        VipParkingStrategy vipParkingStrategy = spy(new VipParkingStrategy());
 
-        Car car=mock(Car.class);
+        Car car = mock(Car.class);
         when(car.getName()).thenReturn("car");
         doReturn(false).when(vipParkingStrategy).isAllowOverPark(car);
 
-        ParkingLot parkingLot=mock(ParkingLot.class);
+        ParkingLot parkingLot = mock(ParkingLot.class);
         doReturn(true).when(parkingLot).isFull();
         when(parkingLot.getName()).thenReturn("parkingLot");
 
         //when
-        vipParkingStrategy.park(Arrays.asList(parkingLot),car);
+        vipParkingStrategy.park(Arrays.asList(parkingLot), car);
 
         //then
-        verify(vipParkingStrategy,times(1)).createNoSpaceReceipt(car);
+        verify(vipParkingStrategy, times(1)).createNoSpaceReceipt(car);
 
     }
 
     @Test
     public void testIsAllowOverPark_givenCarNameContainsCharacterAAndIsVipCar_thenReturnTrue() {
 
-        /* Exercise 5, Write a test case on VipParkingStrategy.isAllowOverPark()
-         * You may refactor the code, or try to use
-         * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
-         */
+        Car car = createMockCar("ACar");
+        vipParkingStrategy.isAllowOverPark(car);
+        when(carDao.isVip(car.getName())).thenReturn(true);
+        //when
+        Boolean isVipParkingStrategy = vipParkingStrategy.isAllowOverPark(car);
+        //then
+        Assert.assertTrue(isVipParkingStrategy);
     }
 
     @Test
